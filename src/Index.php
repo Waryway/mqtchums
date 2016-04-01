50679ccb67197225b0e18072e254e93d98a89f8f
Kyle Wiering <kawiering@yahoo.com>
2016-04-01 00:50:29 -0500

    Basic website - needs much work.

---
+++ src/Index.php
@@ -0,0 +1,23 @@
+<?php
+require_once  '..'. DIRECTORY_SEPARATOR . 'vendor'. DIRECTORY_SEPARATOR . 'autoload.php';
+
+mqtchums\config\Configuration::WEBSITE_NAME;
+
+class Index
+{
+
+	use WarywayWebsiteTemplate\traits\Page;
+	
+	protected function Configure()
+	{
+		$this->setPageName('MQTCHUMS');
+	}
+	
+	
+	protected function RenderBodyContent()
+	{		
+		return 'Welcome to MQTCHUMS.org!';
+	}
+}
+new Index();
+?>
---
+++ src/config/configuration.php
@@ -0,0 +1,8 @@
+<?php
+namespace mqtchums\config;
+
+define('WEBSITE_NAME', 'MQTCHUMS');
+
+class Configuration extends \WarywayWebsiteTemplate\classes\Environment
+{
+}
