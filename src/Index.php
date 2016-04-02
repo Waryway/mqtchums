091e75092524a270e75d50ca43ad8823ea858ca7
Kyle Wiering <kawiering@yahoo.com>
2016-04-02 15:43:35 -0500

    pathing...

--- src/Index.php
+++ src/Index.php
@@ -2,10 +2,10 @@
 namespace mqtchums;
 while(!file_exists(getcwd() . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'))
 {
-    chdir('../');
+    chdir('..'.DIRECTORY_SEPARATOR);
 }
 chdir('src');
-require_once '../'. DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once '..'. DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 \mqtchums\config\Configuration::WEBSITE_NAME;
 
