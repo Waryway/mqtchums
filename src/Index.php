e473bcaa817c113bb9dab9d417ab891f6f789209
Kyle Wiering <kawiering@yahoo.com>
2016-04-06 20:20:54 -0500

    back to auto detect in the tests directory.

--- src/Index.php
+++ src/Index.php
@@ -1,10 +1,6 @@
 <?php
 namespace mqtchums;
-while(!file_exists(getcwd() . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'))
-{
-    chdir('..'.DIRECTORY_SEPARATOR);
-}
-chdir('src');
+
 require_once '..'. DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 \mqtchums\config\Configuration::WEBSITE_NAME;
