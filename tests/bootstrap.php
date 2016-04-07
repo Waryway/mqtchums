e8b91b124d8159e6f0cda9e601d4fb340043b9dd
Kyle Wiering <kawiering@yahoo.com>
2016-04-06 20:57:48 -0500

    pathing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,5 +5,5 @@
  * Date: 4/6/2016
  * Time: 8:52 PM
  */
-require_once __DIR__ . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 ?>
